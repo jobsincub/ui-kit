3c2b649d9a670593aa0592b31b85c8fdb6b5e2f2
Wanderhappen <miracle12301230@gmail.com>
2024-11-14 00:20:21 +0400

    initial commit

---
+++ src/components/button/button.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './button.module.scss'
+
+export const buttonVariant = ['icon', 'link', 'primary', 'secondary', 'tertiary'] as const
+
+export type ButtonVariant = (typeof buttonVariant)[number]
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  fullWidth?: boolean
+  variant?: ButtonVariant
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => {
+  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+
+  const classNames = clsx(s.button, s[variant], fullWidth && s.fullWidth, className)
+
+  return <Component className={classNames} {...rest} />
+}
