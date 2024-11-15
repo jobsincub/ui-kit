c755ef4a9d88b974e7dae20604c63c1b3de972a4
Matvei <motya2222@gmail.com>
2024-11-15 22:21:51 +0300

    chore: add primary font

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,7 @@
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 import type { Preview } from '@storybook/react'
 import '../src/styles/index.scss'
 const preview: Preview = {
