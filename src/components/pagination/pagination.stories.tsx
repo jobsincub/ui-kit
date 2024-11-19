4364e420eedb6c08be0333fa704ddf3e4d6e4f0e
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-11-19 14:49:17 +0300

    docs: add story for Pagination component

---
+++ src/components/pagination/pagination.stories.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Pagination } from './pagination'
+const meta = {
+  component: Pagination,
+  tags: ['autodocs'],
+  title: 'Components/Pagination',
+} as Meta<typeof Pagination>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const Template: Story = {
+  render: args => {
+    const [page, setPage] = useState<number>(args.currentPage || 1)
+    const [perPage, setPerPage] = useState<number>(args.perPage || 10)
+    const totalPages = args.pageAmount || 55
+    const handlePageChange = (page: number) => {
+      setPage(page)
+    }
+
+    const handlePerPageChange = (perPage: number) => {
+      setPerPage(perPage)
+      setPage(1)
+    }
+
+    return (
+      <Pagination
+        currentPage={page}
+        onChange={handlePageChange}
+        onPerPageChange={handlePerPageChange}
+        pageAmount={totalPages}
+        perPage={perPage}
+        perPageOptions={args.perPageOptions}
+      />
+    )
+  },
+}
+
+export const Default: Story = {
+  args: {
+    ...Template.args,
+    perPage: 10,
+    perPageOptions: [10, 20, 30, 50, 100],
+  },
+  render: Template.render,
+}
+
+export const Middle: Story = {
+  args: {
+    ...Template.args,
+    currentPage: 7,
+    perPage: 10,
+    perPageOptions: [10, 20, 30, 50, 100],
+  },
+  render: Template.render,
+}
