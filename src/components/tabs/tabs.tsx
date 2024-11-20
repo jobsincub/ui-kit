66354f97dc450fff320822bb9745565af31901b0
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2024-11-21 01:33:10 +0300

    refactor: apply eslint and prettier

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,49 +1,66 @@
-import {type ComponentPropsWithoutRef, type ElementRef, forwardRef} from 'react'
-import * as TabsPrimitive from '@radix-ui/react-tabs'
-import s from './tabs.module.scss'
+import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
 
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+
+import s from './tabs.module.scss'
 
 export const tabsVariant = ['primary', 'secondary'] as const
 
 export type TabsVariant = (typeof tabsVariant)[number]
 
-
 const Root = forwardRef<
-    ElementRef<typeof TabsPrimitive.Root>,
-    ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
->(({asChild = false, defaultValue, ...props}, ref) => (
-    <TabsPrimitive.Root asChild={asChild} defaultValue = {defaultValue} className={s.Root} ref={ref} {...props} />
+  ElementRef<typeof TabsPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
+>(({ asChild = false, defaultValue, ...props }, ref) => (
+  <TabsPrimitive.Root
+    asChild={asChild}
+    className={s.Root}
+    defaultValue={defaultValue}
+    ref={ref}
+    {...props}
+  />
 ))
 
 Root.displayName = TabsPrimitive.Root.displayName
 
 const List = forwardRef<
-    ElementRef<typeof TabsPrimitive.List>,
-    ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({asChild = false, ...props}, ref) => (
-    <TabsPrimitive.List asChild={asChild} className={s.List} ref={ref} {...props}/>
+  ElementRef<typeof TabsPrimitive.List>,
+  ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ asChild = false, ...props }, ref) => (
+  <TabsPrimitive.List asChild={asChild} className={s.List} ref={ref} {...props} />
 ))
 
 List.displayName = TabsPrimitive.List.displayName
 
 const Trigger = forwardRef<
-    HTMLButtonElement,
-    ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({asChild = false, className = "primary", value, disabled = false, ...props}, ref) => (
-    <TabsPrimitive.Trigger asChild={asChild} value={value} disabled = {disabled} ref={ref} className={s[className] + " " + s.Trigger} {...props} />
+  HTMLButtonElement,
+  ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ asChild = false, className = 'primary', disabled = false, value, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    asChild={asChild}
+    className={s[className] + ' ' + s.Trigger}
+    disabled={disabled}
+    ref={ref}
+    value={value}
+    {...props}
+  />
 ))
 
 Trigger.displayName = TabsPrimitive.Trigger.displayName
 
 const Content = forwardRef<
-    ElementRef<typeof TabsPrimitive.Content>,
-    ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({asChild = false, value, ...props}, ref) => (
-    <TabsPrimitive.Content asChild={asChild} value={value} className={s.Content} ref={ref} {...props}/>
+  ElementRef<typeof TabsPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ asChild = false, value, ...props }, ref) => (
+  <TabsPrimitive.Content
+    asChild={asChild}
+    className={s.Content}
+    ref={ref}
+    value={value}
+    {...props}
+  />
 ))
 
-Content.displayName =TabsPrimitive.Content.displayName
+Content.displayName = TabsPrimitive.Content.displayName
 
-export {Root, List, Content, Trigger}
-
-
+export { Content, List, Root, Trigger }
