41005806b20add8085f2a0e7ae5c056ea8530b9e
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-11-20 18:59:49 +0300

    feat: add Recaptcha component

--- src/recaptcha/recaptcha.tsx
+++ src/recaptcha/recaptcha.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import { ReCAPTCHAProps } from 'react-google-recaptcha'
 
-export const Recaptcha = () => {
-  return <div>recaptcha</div>
+import clsx from 'clsx'
+
+import s from './recaptcha.module.scss'
+
+type Props = {
+  error: string
+} & Omit<ReCAPTCHAProps, 'sitekey'>
+
+export const Recaptcha = ({ children, className, error }: Props) => {
+  return (
+    <div className={clsx(error && s.errorBox, className)}>
+      {children}
+      {error && <div className={s.error}>{error}</div>}
+    </div>
+  )
 }
