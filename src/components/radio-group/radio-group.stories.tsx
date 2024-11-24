0c8f87115f14ec2ff88891208a79e0e84c98a21c
Matvei <motya2222@gmail.com>
2024-11-24 17:45:03 +0300

    refactor: change RadioGroupItem stories

--- src/components/radio-group/radio-group.stories.tsx
+++ src/components/radio-group/radio-group.stories.tsx
@@ -1,49 +1,92 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import React from 'react'
+import { useState } from 'react'
 
-import { RadioGroup, RadioGroupIndicator, RadioGroupItem } from './'
-import { CheckedIcon } from './assets/icons/CheckedIcon'
-import { UncheckedIcon } from './assets/icons/UncheckedIcon'
+import { RadioGroup, RadioGroupItem } from './'
 
-const meta: Meta<typeof RadioGroup> = {
+const meta = {
+  argTypes: {
+    asChild: { control: 'boolean', defaultValue: false },
+    disabled: { control: 'boolean', defaultValue: false },
+    name: {
+      description: 'The name used when using this component inside a form',
+    },
+    onValueChange: { description: '(value: string) => void' },
+    value: { description: 'string' },
+  },
   component: RadioGroup,
+  tags: ['autodocs'],
   title: 'Components/RadioGroup',
-}
+} satisfies Meta<typeof RadioGroup>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  render: () => (
-    <RadioGroup aria-label={'View density'} defaultValue={'default'}>
-      <div style={{ alignItems: 'center', display: 'flex' }}>
-        <RadioGroupItem id={'r1'} value={'default'}>
-          <UncheckedIcon />
-          <RadioGroupIndicator>
-            <CheckedIcon />
-          </RadioGroupIndicator>
-        </RadioGroupItem>
-        <label htmlFor={'r1'}>Default</label>
-      </div>
-      <div style={{ alignItems: 'center', display: 'flex' }}>
-        <RadioGroupItem id={'r2'} value={'comfortable'}>
-          <UncheckedIcon />
-          <RadioGroupIndicator>
-            <CheckedIcon />
-          </RadioGroupIndicator>
-        </RadioGroupItem>
-        <label>Comfortable</label>
-      </div>
-      <div style={{ alignItems: 'center', display: 'flex' }}>
-        <RadioGroupItem id={'r3'} value={'compact'}>
-          <UncheckedIcon />
-          <RadioGroupIndicator>
-            <CheckedIcon />
-          </RadioGroupIndicator>
-        </RadioGroupItem>
-        <label htmlFor={'r3'}>Compact</label>
-      </div>
-    </RadioGroup>
-  ),
+  args: {
+    disabled: false,
+  },
+  render: args => {
+    const items = [
+      { id: 'r1', label: 'Default', value: 'default' },
+      { id: 'r2', label: 'Comfortable', value: 'comfortable' },
+      { id: 'r3', label: 'Compact', value: 'compact' },
+    ]
+    const [value, setValue] = useState<string>()
+
+    console.log({ value })
+
+    return (
+      <>
+        <RadioGroup {...args} onValueChange={setValue} value={value}>
+          {items.map(item => (
+            <div
+              key={item.id}
+              style={{
+                alignItems: 'center',
+                display: 'flex',
+              }}
+            >
+              <RadioGroupItem
+                htmlFor={item.id}
+                id={item.id}
+                label={item.label}
+                value={item.value}
+              />
+            </div>
+          ))}
+        </RadioGroup>
+        <div>Selected value: {value}</div>
+      </>
+    )
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  render: args => {
+    const items = [
+      { id: 'r4', label: 'Disabled 1', value: 'disabled1' },
+      { id: 'r5', label: 'Disabled 2', value: 'disabled2' },
+      { id: 'r6', label: 'Disabled 3', value: 'disabled3' },
+    ]
+
+    return (
+      <RadioGroup {...args}>
+        {items.map(item => (
+          <div
+            key={item.id}
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+            }}
+          >
+            <RadioGroupItem htmlFor={item.id} id={item.id} label={item.label} value={item.value} />
+          </div>
+        ))}
+      </RadioGroup>
+    )
+  },
 }
