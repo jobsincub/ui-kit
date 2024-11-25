504e0652ac07e169492fccc41ae339707c771c5a
Wanderhappen <miracle12301230@gmail.com>
2024-11-25 17:22:57 +0400

    refactor

--- src/components/datePicker/datePicker.tsx
+++ src/components/datePicker/datePicker.tsx
@@ -2,57 +2,45 @@ import type { PropsRangeRequired, PropsSingleRequired } from 'react-day-picker'
 
 import { useState } from 'react'
 
+import { clsx } from 'clsx'
+
 import s from './datePicker.module.scss'
 
 import { Calendar } from '../calendar'
 import { Popover, PopoverContent, PopoverTrigger } from '../popover'
-import { DateFormatter } from './date'
+import { DateFormatter, validateDate } from './date'
 import { ErrorMessage } from './erorrMessage'
 
-type DatePickerProps =
-  | Pick<PropsRangeRequired, 'mode' | 'onSelect' | 'selected'>
-  | Pick<PropsSingleRequired, 'mode' | 'onSelect' | 'selected'>
+type SingleModeProps = Pick<PropsSingleRequired, 'mode' | 'onSelect' | 'selected'>
+type RangeModeProps = Pick<PropsRangeRequired, 'mode' | 'onSelect' | 'selected'>
+type DatePickerProps = { disabled?: boolean } & (RangeModeProps | SingleModeProps)
 
-export const DatePicker = (props: DatePickerProps) => {
+export const DatePicker = ({ disabled = false, ...props }: DatePickerProps) => {
   const [active, setActive] = useState(false)
   const [error, setError] = useState(false)
 
-  const className = `${s.button} ${active ? s.button_active : ''} ${
-    props.mode === 'single' ? s.single : s.range
-  } ${error && s.button_error}`
-
   const selectHandler = (selectedDate: Date) => {
-    if (!selectedDate) {
-      return
-    }
-
-    const today = new Date()
-    const currentMonth = today.getMonth()
-    const previousMonth = currentMonth === 0 ? 11 : currentMonth - 1
-    const selectedMonth = selectedDate.getMonth()
-    const selectedYear = selectedDate.getFullYear()
-
-    if (
-      selectedYear !== today.getFullYear() &&
-      !(selectedYear === today.getFullYear() - 1 && selectedMonth === 11)
-    ) {
-      setError(true)
-
-      return
-    }
-
-    if (selectedMonth !== currentMonth && selectedMonth !== previousMonth) {
-      setError(true)
-    } else {
-      setError(false)
-    }
+    setError(!validateDate(selectedDate))
   }
+
+  const className = clsx(s.button, {
+    [s.button_active]: active,
+    [s.button_disabled]: disabled,
+    [s.button_error]: error,
+    [s.range]: props.mode === 'range',
+    [s.single]: props.mode === 'single',
+  })
 
   return (
     <Popover onOpenChange={setActive}>
       <div className={s.wrapper}>
         <PopoverTrigger asChild={false} className={className}>
-          <DateFormatter className={s.dateFormatter} date={props.selected} error={error} />
+          <DateFormatter
+            className={s.dateFormatter}
+            date={props.selected}
+            disabled={disabled}
+            error={error}
+          />
         </PopoverTrigger>
         {error && <ErrorMessage mode={props.mode} />}
       </div>
