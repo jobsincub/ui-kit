c89da3d600c48dd2ca171c8e088b1e9947aeb20a
Matvei <motya2222@gmail.com>
2024-11-27 12:43:41 +0300

    feat: add searchIcon support

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,14 +4,17 @@ import { clsx } from 'clsx'
 
 import s from './input.module.scss'
 
+import SearchIcon from '../../assets/icons/components/common/search'
+
 export type InputProps = React.ComponentProps<'input'> & {
   error?: string
   label?: string
 }
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const { className, error, id, label, ...rest } = props
+  const { className, error, id, label, type, ...rest } = props
   const classNames = clsx(s.input, className)
+  const isSearchType = type === 'search'
 
   return (
     <div>
@@ -20,7 +23,10 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref)
           {label}
         </label>
       )}
-      <input className={classNames} ref={ref} {...rest} />
+      <div>
+        {isSearchType && <SearchIcon />}
+        <input className={classNames} id={id} ref={ref} type={type} {...rest} />
+      </div>
       {error && <span className={s.errorText}>{error}</span>}
     </div>
   )
