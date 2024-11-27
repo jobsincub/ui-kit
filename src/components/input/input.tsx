13009c520582f222cbcec2feb973ffc10dc1d688
Matvei <motya2222@gmail.com>
2024-11-27 14:19:31 +0300

    feat: add eyes icons support for input type password

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { clsx } from 'clsx'
 
 import s from './input.module.scss'
 
+import EyeIcon from '../../assets/icons/components/common/eye'
+import EyeOffIcon from '../../assets/icons/components/common/eye-off'
 import SearchIcon from '../../assets/icons/components/common/search'
 
 export type InputProps = React.ComponentProps<'input'> & {
@@ -15,6 +17,12 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref)
   const { className, error, id, label, type, ...rest } = props
   const classNames = clsx(s.input, className)
   const isSearchType = type === 'search'
+  const isPasswordType = type === 'password'
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(prev => !prev)
+  }
 
   return (
     <div className={s.wrapper}>
@@ -25,7 +33,18 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref)
       )}
       <div className={s.inputWrapper}>
         {isSearchType && <SearchIcon className={s.searchIcon} />}
-        <input className={classNames} id={id} ref={ref} type={type} {...rest} />
+        <input
+          className={classNames}
+          id={id}
+          ref={ref}
+          type={isPasswordType && isPasswordVisible ? 'text' : type}
+          {...rest}
+        />
+        {isPasswordType && (
+          <button className={s.eyeButton} onClick={togglePasswordVisibility}>
+            {isPasswordVisible ? <EyeIcon /> : <EyeOffIcon />}
+          </button>
+        )}
       </div>
       {error && <span className={s.errorText}>{error}</span>}
     </div>
