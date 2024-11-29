4867787753bdfb1a772b03bf3fc22900e6e8b727
Matvei <motya2222@gmail.com>
2024-11-29 21:01:08 +0300

    feat: add basic styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,15 @@
 import { ComponentProps, forwardRef } from 'react'
 
+import { clsx } from 'clsx'
+
+import s from './header.module.scss'
+
 export type HeaderProps = ComponentProps<'header'>
 
-export const Header = forwardRef<HTMLElement, HeaderProps>(({ children, ...props }, ref) => (
-  <header ref={ref} {...props}>
-    {children}
-  </header>
-))
+export const Header = forwardRef<HTMLElement, HeaderProps>(
+  ({ children, className, ...props }, ref) => (
+    <header className={clsx(s.header, className)} ref={ref} {...props}>
+      {children}
+    </header>
+  )
+)
