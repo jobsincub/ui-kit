eb7b3a181834fe4df846b8d764d80090b95b5a60
Wanderhappen <miracle12301230@gmail.com>
2024-12-03 00:07:48 +0400

    refactor: apply eslint for icons

--- src/assets/icons/components/common/arrow-right.tsx
+++ src/assets/icons/components/common/arrow-right.tsx
@@ -5,17 +5,17 @@ const SvgComponent = (
   ref: Ref<SVGSVGElement>
 ) => (
   <svg
-    fill="currentColor"
+    fill={'currentColor'}
     height={height}
     ref={ref}
-    viewBox="0 0 7 10"
+    viewBox={'0 0 7 10'}
     width={width}
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns={'http://www.w3.org/2000/svg'}
     {...rest}
   >
     <path
-      d="M1.99999 10L0.937988 8.938L4.87499 5L0.937988 1.062L1.99999 0L6.99999 5L1.99999 10Z"
-      fill="currentColor"
+      d={'M1.99999 10L0.937988 8.938L4.87499 5L0.937988 1.062L1.99999 0L6.99999 5L1.99999 10Z'}
+      fill={'currentColor'}
     />
   </svg>
 )
