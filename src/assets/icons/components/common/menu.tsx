52d6c72b659bd9c7c19953fce0d5beefe567a279
Ayan <nazbievayan16@gmail.com>
2024-12-09 13:09:11 +0500

    eslint

--- src/assets/icons/components/common/menu.tsx
+++ src/assets/icons/components/common/menu.tsx
@@ -5,20 +5,24 @@ const SvgComponent = (
   ref: Ref<SVGSVGElement>
 ) => (
   <svg
-    fill="currentColor"
+    fill={'currentColor'}
     height={height}
     ref={ref}
-    viewBox="0 0 24 24"
+    viewBox={'0 0 24 24'}
     width={width}
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns={'http://www.w3.org/2000/svg'}
     {...rest}
   >
-    <g clipPath="url(#a)" fill="currentColor">
-      <path d="M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM19 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM5 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+    <g clipPath={'url(#a)'} fill={'currentColor'}>
+      <path
+        d={
+          'M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM19 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM5 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'
+        }
+      />
     </g>
     <defs>
-      <clipPath id="a">
-        <path d="M0 0h24v24H0z" fill="#fff" />
+      <clipPath id={'a'}>
+        <path d={'M0 0h24v24H0z'} fill={'#fff'} />
       </clipPath>
     </defs>
   </svg>
