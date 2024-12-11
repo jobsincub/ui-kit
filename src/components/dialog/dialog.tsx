0f527fa2faec87cf471576e0ae602d4f6cf5b1aa
Matvei <motya2222@gmail.com>
2024-12-11 18:50:43 +0300

    refactor: import React types and forwardRef directly instead using embedded links

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import { ComponentPropsWithoutRef, ElementRef, HTMLAttributes, forwardRef } from 'react'
 
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { clsx } from 'clsx'
@@ -9,34 +9,34 @@ import CloseIcon from '../../assets/icons/components/common/close'
 
 const Dialog = DialogPrimitive.Root
 
-const DialogTrigger = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Trigger>
+const DialogTrigger = forwardRef<
+  ElementRef<typeof DialogPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Trigger>
 >(({ asChild = true, ...props }, ref) => (
   <DialogPrimitive.Trigger asChild={asChild} ref={ref} {...props} />
 ))
 
 const DialogPortal = DialogPrimitive.Portal
 
-const DialogClose = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Close>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
+const DialogClose = forwardRef<
+  ElementRef<typeof DialogPrimitive.Close>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
 >(({ asChild = true, ...props }, ref) => (
   <DialogPrimitive.Close asChild={asChild} ref={ref} {...props} />
 ))
 
-const DialogOverlay = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+const DialogOverlay = forwardRef<
+  ElementRef<typeof DialogPrimitive.Overlay>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay className={clsx(s.overlay, className)} ref={ref} {...props} />
 ))
 
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
-const DialogContent = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+const DialogContent = forwardRef<
+  ElementRef<typeof DialogPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
@@ -46,9 +46,9 @@ const DialogContent = React.forwardRef<
 
 DialogContent.displayName = DialogPrimitive.Content.displayName
 
-const DialogCloseIcon = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Close>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
+const DialogCloseIcon = forwardRef<
+  ElementRef<typeof DialogPrimitive.Close>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Close className={clsx(s.close, className)} ref={ref} {...props}>
     <CloseIcon className={s.closeIcon} />
@@ -58,7 +58,7 @@ const DialogCloseIcon = React.forwardRef<
 
 DialogCloseIcon.displayName = 'DialogCloseIcon'
 
-type DialogHeaderProps = React.HTMLAttributes<HTMLDivElement> & {
+type DialogHeaderProps = HTMLAttributes<HTMLDivElement> & {
   isCloseIconVisible?: boolean
 }
 
@@ -76,30 +76,30 @@ const DialogHeader = ({
 
 DialogHeader.displayName = 'DialogHeader'
 
-const DialogBody = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+const DialogBody = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
   <div className={clsx(s.body, className)} {...props} />
 )
 
 DialogBody.displayName = 'DialogBody'
 
-const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+const DialogFooter = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => (
   <div className={clsx(s.footer, className)} {...props} />
 )
 
 DialogFooter.displayName = 'DialogFooter'
 
-const DialogTitle = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Title>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+const DialogTitle = forwardRef<
+  ElementRef<typeof DialogPrimitive.Title>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Title className={clsx(s.title, className)} ref={ref} {...props} />
 ))
 
 DialogTitle.displayName = DialogPrimitive.Title.displayName
 
-const DialogDescription = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Description>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
+const DialogDescription = forwardRef<
+  ElementRef<typeof DialogPrimitive.Description>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description className={clsx(s.description, className)} ref={ref} {...props} />
 ))
