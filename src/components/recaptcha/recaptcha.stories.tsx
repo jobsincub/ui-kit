fce625f6cc19e9de5a7b9b5e3e8488bcdc7a5e7c
Matvei <motya2222@gmail.com>
2025-06-24 14:19:11 +0300

    refactor: change sitekey value

--- src/components/recaptcha/recaptcha.stories.tsx
+++ src/components/recaptcha/recaptcha.stories.tsx
@@ -11,16 +11,18 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof Recaptcha>
 
+const sitekey = '6LdDzWorAAAAAPcxJrGwISEs6xoa8Pw4Zy1jCbmP'
+
 export const Default: Story = {
   args: {
     onChange: token => console.log('Verified:', token),
-    sitekey: '6LcrEYQqAAAAAH39ZWiTbCHde_Z_-83OBxIXB7l8', // тестовый ключ
+    sitekey,
   },
 }
 export const RecaptchaWithError: Story = {
   args: {
     error: 'Please verify that you are not a robot',
     onChange: token => console.log('Verified:', token),
-    sitekey: '6LcrEYQqAAAAAH39ZWiTbCHde_Z_-83OBxIXB7l8', // тестовый ключ
+    sitekey,
   },
 }
